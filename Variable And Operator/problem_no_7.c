aea758487382d87482861ba350301d578a4a2d7f
Suresh Dhole <sureshdhole226@gmail.com>
2026-05-26 18:01:11 +0530

    Q7.    WAP to check if a number is even or odd using % operator.

---
+++ Variable And Operator/problem_no_7.c
@@ -0,0 +1,19 @@
+// 7.	WAP to check if a number is even or odd using % operator.
+#include <stdio.h>
+int main()
+{
+    int num;
+
+    printf("Enter the number :");
+    scanf("%d", &num);
+
+    if (num % 2 == 0)
+    {
+        printf("The %d is even ", num);
+    }
+    else
+    {
+        printf("The %d is odd ", num);
+    }
+    return 0;
+}
