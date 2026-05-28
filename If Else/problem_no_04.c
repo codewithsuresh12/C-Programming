b2d3b640c9ce355d0cbb6b83b617c07ee59430f1
Suresh Dhole <sureshdhole226@gmail.com>
2026-05-28 20:00:01 +0530

    4. Greatest of 2 numbers

---
+++ If Else/problem_no_04.c
@@ -0,0 +1,22 @@
+// 4. Greatest of 2 numbers: 2 number me bada konsa
+#include <stdio.h>
+int main()
+{
+    int a, b;
+
+    printf("Enter the a :");
+    scanf("%d", &a);
+
+    printf("Enter the b :");
+    scanf("%d", &b);
+
+    if (a > b)
+    {
+        printf("A is Gretest\n");
+    }
+    else
+    {
+        printf("B is gretest\n");
+    }
+    return 0;
+}
