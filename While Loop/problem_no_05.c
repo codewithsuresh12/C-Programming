3eff3c1982bcb93d50d7f1c8b000496e28beafe4
Suresh Dhole <sureshdhole226@gmail.com>
2026-06-03 17:58:08 +0530

    Q5: Write a program to find the sum of first 10 natural numbers using while loop.

---
+++ While Loop/problem_no_05.c
@@ -0,0 +1,17 @@
+// Q5: Write a program to find the sum of first 10 natural numbers using while loop.
+#include <stdio.h>
+
+int main()
+{
+    int i = 1, sum = 0;
+
+    while (i <= 10)
+    {
+        sum += i;
+        i++;
+    }
+
+    printf("Sum = %d", sum);
+
+    return 0;
+}
