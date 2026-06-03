c0a62a1b5b56ceb86df0c61206e0c714da9c22e0
Suresh Dhole <sureshdhole226@gmail.com>
2026-06-03 17:59:05 +0530

    Q7: Write a program to calculate factorial of a number using while loop.

---
+++ While Loop/problem_no_08.c
@@ -0,0 +1,21 @@
+// Q8: Write a program to reverse a number using while loop.
+#include <stdio.h>
+
+int main()
+{
+    int num, reverse = 0, rem;
+
+    printf("Enter Number: ");
+    scanf("%d", &num);
+
+    while (num != 0)
+    {
+        rem = num % 10;
+        reverse = reverse * 10 + rem;
+        num = num / 10;
+    }
+
+    printf("Reverse = %d", reverse);
+
+    return 0;
+}
