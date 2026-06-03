43b0208b0d5025976fa440775fc1284a52598eaf
Suresh Dhole <sureshdhole226@gmail.com>
2026-06-03 17:57:11 +0530

    Q2: Write a program to print numbers from 10 to 1 using while loop.

---
+++ While Loop/problem_no_02.c
@@ -0,0 +1,16 @@
+// Q2: Write a program to print numbers from 10 to 1 using while loop.
+
+#include <stdio.h>
+
+int main()
+{
+    int i = 10;
+
+    while (i >= 1)
+    {
+        printf("%d\n", i);
+        i--;
+    }
+
+    return 0;
+}
