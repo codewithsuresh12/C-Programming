2e7d211ad015c087903e23f5c219c5697814a160
Suresh Dhole <sureshdhole226@gmail.com>
2026-06-05 12:42:28 +0530

    1. WAP to Print Numbers from 1 to 10 using do-while loop

---
+++ Do While Loop/problem_no_01.c
@@ -0,0 +1,15 @@
+// 1. WAP to Print Numbers from 1 to 10 using do-while loop
+#include <stdio.h>
+
+int main()
+{
+    int i = 1;
+
+    do
+    {
+        printf("%d\n", i);
+        i++;
+    } while (i <= 10);
+
+    return 0;
+}
